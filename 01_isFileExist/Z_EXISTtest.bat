4745ac650424cbe2ae509c9b26d9222a40c7f57d
gonechar <gonechar@gmail.com>
2015-03-07 02:20:55 +0600

    07.03.02_20
    ADDITION OF ARGS PARSER CLASS

---
+++ 01_isFileExist/Z_EXISTtest.bat
@@ -0,0 +1,30 @@
+DEL e:\PROG\LR1_U_TEST\01_isFileExist\01_EXISTtest.log
+
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\genTests\GENERATE1.done.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\genTests\generate2.done.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\genTests\Generate3.done.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\genTests\CREATE1.done.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\genTests\create2.done.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\genTests\Create3.done.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\genTests\Create.FAULT.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+
+
+
+
--- reset.bat
+++ reset.bat
@@ -1,53 +1,55 @@
 :: Очистка всех логов
-DEL e:\PROG\LR1\reset.log
+DEL e:\PROG\LR1_U_TEST\reset.log
 
-DEL e:\PROG\LR1\genTests\01_GENtest.log
-DEL e:\PROG\LR1\delTests\01_DELtest.log
-DEL e:\PROG\LR1\copyTests\01_COPYtest.log
-DEL e:\PROG\LR1\renTests\01_RENtest.log
+DEL e:\PROG\LR1_U_TEST\genTests\01_GENtest.log
+DEL e:\PROG\LR1_U_TEST\delTests\01_DELtest.log
+DEL e:\PROG\LR1_U_TEST\copyTests\01_COPYtest.log
+DEL e:\PROG\LR1_U_TEST\renTests\01_RENtest.log
+
+DEL e:\PROG\LR1_U_TEST\01_isFileExist\01_EXISTtest.log
 
 
 :: GENERATE
 :: Удаление всех созданных файлов
-DEL e:\PROG\LR1\genTests\GENERATE1.done.exe >> reset.log
-DEL e:\PROG\LR1\genTests\generate2.done.exe >> reset.log
-DEL e:\PROG\LR1\genTests\Generate3.done.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\genTests\GENERATE1.done.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\genTests\generate2.done.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\genTests\Generate3.done.exe >> reset.log
 
 
-DEL e:\PROG\LR1\genTests\CREATE1.done.exe >> reset.log
-DEL e:\PROG\LR1\genTests\create2.done.exe >> reset.log
-DEL e:\PROG\LR1\genTests\Create3.done.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\genTests\CREATE1.done.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\genTests\create2.done.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\genTests\Create3.done.exe >> reset.log
 
 ::DELETE
 :: Восстановление 6 удаленных тестовых файлов
 :: 7-ой на всякий случай, но он не должен удаляться согласно тесту
-e:\PROG\LR1\tp.lab1.exe CREATE e:\PROG\LR1\delTests\DELETE.1.destroy.exe >> reset.log
-e:\PROG\LR1\tp.lab1.exe CREATE e:\PROG\LR1\delTests\delete.2.destroy.exe >> reset.log
-e:\PROG\LR1\tp.lab1.exe CREATE e:\PROG\LR1\delTests\Delete.3.destroy.exe >> reset.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe CREATE e:\PROG\LR1_U_TEST\delTests\DELETE.1.destroy.exe >> reset.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe CREATE e:\PROG\LR1_U_TEST\delTests\delete.2.destroy.exe >> reset.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe CREATE e:\PROG\LR1_U_TEST\delTests\Delete.3.destroy.exe >> reset.log
 
-e:\PROG\LR1\tp.lab1.exe CREATE e:\PROG\LR1\delTests\DEL.1.destroy.exe >> reset.log
-e:\PROG\LR1\tp.lab1.exe CREATE e:\PROG\LR1\delTests\del.2.destroy.exe >> reset.log
-e:\PROG\LR1\tp.lab1.exe CREATE e:\PROG\LR1\delTests\Del.3.destroy.exe >> reset.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe CREATE e:\PROG\LR1_U_TEST\delTests\DEL.1.destroy.exe >> reset.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe CREATE e:\PROG\LR1_U_TEST\delTests\del.2.destroy.exe >> reset.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe CREATE e:\PROG\LR1_U_TEST\delTests\Del.3.destroy.exe >> reset.log
 
-e:\PROG\LR1\tp.lab1.exe CREATE e:\PROG\LR1\delTests\DELIM.destroy.exe >> reset.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe CREATE e:\PROG\LR1_U_TEST\delTests\DELIM.destroy.exe >> reset.log
 
 ::COPY
 :: Удаляем копии файлов сделанных во время тестов
-DEL e:\PROG\LR1\copyTests\COPY.1.SUCCESS.exe >> reset.log
-DEL e:\PROG\LR1\copyTests\copy.2.SUCCESS.exe >> reset.log
-DEL e:\PROG\LR1\copyTests\Copy.3.SUCCESS.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\copyTests\COPY.1.SUCCESS.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\copyTests\copy.2.SUCCESS.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\copyTests\Copy.3.SUCCESS.exe >> reset.log
 
 ::RENAME
 :: Переименовываем обратно
-e:\PROG\LR1\tp.lab1.exe RENAME e:\PROG\LR1\renTests\RENAME.1.SUCCESS.exe e:\PROG\LR1\renTests\RENAME.1.exe
-e:\PROG\LR1\tp.lab1.exe rename e:\PROG\LR1\renTests\rename.2.SUCCESS.exe e:\PROG\LR1\renTests\rename.2.exe
-e:\PROG\LR1\tp.lab1.exe Rename e:\PROG\LR1\renTests\Rename.3.SUCCESS.exe e:\PROG\LR1\renTests\Rename.3.exe
+e:\PROG\LR1_U_TEST\tp.lab1.exe RENAME e:\PROG\LR1_U_TEST\renTests\RENAME.1.SUCCESS.exe e:\PROG\LR1_U_TEST\renTests\RENAME.1.exe
+e:\PROG\LR1_U_TEST\tp.lab1.exe rename e:\PROG\LR1_U_TEST\renTests\rename.2.SUCCESS.exe e:\PROG\LR1_U_TEST\renTests\rename.2.exe
+e:\PROG\LR1_U_TEST\tp.lab1.exe Rename e:\PROG\LR1_U_TEST\renTests\Rename.3.SUCCESS.exe e:\PROG\LR1_U_TEST\renTests\Rename.3.exe
 
-e:\PROG\LR1\tp.lab1.exe REANIMATE e:\PROG\LR1\renTests\REANIMATE.FAULT_Rename_ERROR_ERROR.exe e:\PROG\LR1\renTests\REANIMATE.FAULT.exe
+e:\PROG\LR1_U_TEST\tp.lab1.exe REANIMATE e:\PROG\LR1_U_TEST\renTests\REANIMATE.FAULT_Rename_ERROR_ERROR.exe e:\PROG\LR1_U_TEST\renTests\REANIMATE.FAULT.exe
 
 
 
-DEL e:\PROG\LR1\genTests\Create.FAULT.exe >> reset.log
+DEL e:\PROG\LR1_U_TEST\genTests\Create.FAULT.exe >> reset.log
 
 
 
