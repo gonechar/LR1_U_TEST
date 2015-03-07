51125e6e76b5a44611fce43b4b366e4406a8ba78
gonechar <gonechar@gmail.com>
2015-03-07 12:14:26 +0600

    1234
    1234

--- 01_isFileExist/Z_EXISTtest.bat
+++ 01_isFileExist/Z_EXISTtest.bat
@@ -1,6 +1,7 @@
 DEL e:\PROG\LR1_U_TEST\01_isFileExist\01_EXISTtest.log
 
-
+::Постпроверка создания файлов
+::-------------------------------------------------------------
 e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\genTests\GENERATE1.done.exe >> 01_EXISTtest.log
 e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
 
@@ -22,7 +23,36 @@ e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
 e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\genTests\Create.FAULT.exe >> 01_EXISTtest.log
 e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
 
+::-------------------------------------------------------------
 e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+::-------------------------------------------------------------
+
+::Постпроверка удаления файлов
+::-------------------------------------------------------------
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\delTests\DEL.1.destroy.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\delTests\del.2.destroy.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\delTests\Del.3.destroy.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\delTests\DELETE.1.destroy.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\delTests\delete.2.destroy.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\delTests\Delete.3.destroy.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+e:\PROG\LR1_U_TEST\tp.lab1.exe ISEXIST e:\PROG\LR1_U_TEST\delTests\DELIM.destroy.exe >> 01_EXISTtest.log
+e:\PROG\LR1_U_TEST\tp.lab1.exe ABZ >> 01_EXISTtest.log
+
+
 
 
 
